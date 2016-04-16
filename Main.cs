fcf9ae0bb2b912aaf282b965d70435817fc58d7a
zeeth99 <zeeth.max@hotmail.co.nz>
2016-04-16 15:35:15 +1200

    Added movement system

--- Main.cs
+++ Main.cs
@@ -1,16 +1,23 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Main : MonoBehaviour {
 
-	// Use this for initialization
-	void Start()
-	{
+	public InputHandler inputHandler;
+	public EntityPlayer entityPlayer;
 
+	// Use this for initialization
+	void Start() {
+		inputHandler = new GameObject("inputHandler").AddComponent<InputHandler>();
+		inputHandler.main = this;
+
+		entityPlayer = new GameObject("player").AddComponent<EntityPlayer>();
+		Camera.main.transform.parent = entityPlayer.transform;
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 
 	}
+
 }
