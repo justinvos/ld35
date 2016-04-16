84e2f4b3a0f8a2a4475f3eee2e7186c6a5fa2f00
zeeth99 <zeeth.max@hotmail.co.nz>
2016-04-16 15:33:36 +1200

    Revert "Added movement system"
    This reverts commit 14e23a44aefb450c70c85fa1cd0098d16f6c0883.

--- Main.cs
+++ Main.cs
@@ -1,23 +1,16 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class Main : MonoBehaviour {
 
-	public InputHandler inputHandler;
-	public EntityPlayer entityPlayer;
-
 	// Use this for initialization
-	void Start() {
-		inputHandler = new GameObject("inputHandler").AddComponent<InputHandler>();
-		inputHandler.main = this;
+	void Start()
+	{
 
-		entityPlayer = new GameObject("player").AddComponent<EntityPlayer>();
-		Camera.main.transform.parent = entityPlayer.transform;
 	}
 
 	// Update is called once per frame
-	void Update() {
+	void Update () {
 
 	}
-
 }
